bdfd9a784c8258d2cb352f963098998849eab0a2
remi calizzano <remi.calizzano@gmail.com>
2020-06-02 13:10:28 +0200

    Add T5 with title

--- baselines/baselines.py
+++ baselines/baselines.py
@@ -3,6 +3,7 @@ from baselines.lead import Lead
 from baselines.lexrank import LexRank
 from baselines.bart import Bart
 from baselines.t5 import T5
+from baselines.t5_with_title import T5WithTitle
 
 # from baselines.mmr import mmr
 # from baselines.tfidf import TFIDF
@@ -19,4 +20,6 @@ def use(baseline_class, **init_kwargs):
         return Bart(**init_kwargs)
     if baseline_class == "T5":
         return T5(**init_kwargs)
+    if baseline_class == "T5 with title":
+        return T5WithTitle(**init_kwargs)
     raise ValueError("Baseline baseline_class not correct")
